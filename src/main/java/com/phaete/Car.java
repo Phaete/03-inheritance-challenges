9cfbba7450f73a464ea9177032b9b3250aa5c2f5
Phaete <pel4nk@gmail.com>
2024-08-26 11:32:06 +0200

    Initial commit

---
+++ src/main/java/com/phaete/Car.java
@@ -0,0 +1,24 @@
+package com.phaete;
+
+public class Car extends Vehicle{
+    private int numberOfDoors;
+
+    public Car(String manufacturer, String model, int yearOfManufacture, int numberOfDoors) {
+        super(manufacturer, model, yearOfManufacture);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Additionally, it has " +
+                numberOfDoors + " doors";
+    }
+}
---
+++ src/main/java/com/phaete/Main.java
@@ -0,0 +1,12 @@
+package com.phaete;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle("Seat", "Ibiza", 2005);
+        Car car = new Car("Ford", "Mustang", 2023, 3);
+
+        System.out.println(vehicle);
+        System.out.println(car);
+    }
+}
