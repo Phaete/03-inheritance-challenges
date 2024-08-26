68eb697d83df9c011e79c81fe244e260bc9dae6e
Phaete <pel4nk@gmail.com>
2024-08-26 11:42:59 +0200

    Add class Motorcycle and it's boilerplate code

--- src/main/java/com/phaete/Main.java
+++ src/main/java/com/phaete/Main.java
@@ -8,5 +8,25 @@ public class Main {
 
         System.out.println(vehicle);
         System.out.println(car);
+
+        // Bonus
+        Motorcycle motorcycle = new Motorcycle("Kawasaki", "Ninja", 2022, "Sportbike");
+        System.out.println(motorcycle);
     }
+
+    /*
+    Create another derived class from "Vehicle" called "Motorcycle".
+    Add appropriate properties to this class.
+
+    Implement a method in the "Motorcycle" class that returns the type
+    of motorcycle (e.g. Chopper, Sportbike, Cruiser).
+
+    Create instances of both "Car" and "Motorcycle" and call the methods
+    to output the information.
+
+    Discuss together how inheritance was applied in this scenario and what
+    advantages it offers.
+
+
+     */
 }
---
+++ src/main/java/com/phaete/Motorcycle.java
@@ -0,0 +1,31 @@
+package com.phaete;
+
+import java.util.Objects;
+
+public class Motorcycle extends Vehicle {
+    String typeOfMotorcycle;
+
+    public Motorcycle(String manufacturer, String model, int yearOfManufacture, String typeOfMotorcycle) {
+        super(manufacturer, model, yearOfManufacture);
+        this.typeOfMotorcycle = typeOfMotorcycle;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Motorcycle that = (Motorcycle) o;
+        return Objects.equals(typeOfMotorcycle, that.typeOfMotorcycle);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " It is a motorcycle of type " + typeOfMotorcycle + ".";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), typeOfMotorcycle);
+    }
+}
